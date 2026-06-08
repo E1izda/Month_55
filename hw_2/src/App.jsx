3792989cc38f8e85e903ace8ff802ba6185ce6bb
E1izda <elizdademo@gmail.com>
2026-06-08 11:20:17 +0600

    hw2

---
+++ hw_2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ViewUsers from './components/ViewUsers/ViewUsers';
+
+
+const App = () => {
+    return (
+        <div>
+            <ViewUsers />
+        </div>
+    )
+}
+
+export default App
